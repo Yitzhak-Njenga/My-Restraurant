908e8623b5f8103ea46c3f6c4a13fff914143654
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2021-01-06 01:11:05 +0300

    implemeted butter knife

--- app/src/main/java/com/moringaschool/myrestraurants/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestraurants/MainActivity.java
@@ -1,33 +1,77 @@
 package com.moringaschool.myrestraurants;
 
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.view.View;
+//import android.widget.ArrayAdapter;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.ListView;
+//import android.widget.Toast;
+//
+//import butterknife.ButterKnife;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class MainActivity extends AppCompatActivity {
+//    private static final String TAG = MainActivity.class.getSimpleName();
+//    private Button mFindRestaurantsButton;
+//    private EditText mLocationEditText;
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//
+//        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+//        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+//        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+//                String location = mLocationEditText.getText().toString();
+//                intent.putExtra("location", location);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+//}
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
-    private Button mFindRestaurantsButton;
-    private EditText mLocationEditText;
+    @BindView(R.id.findRestaurantsButton)
+    Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText)
+    EditText mLocationEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+        ButterKnife.bind(this);
 
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String location = mLocationEditText.getText().toString();
-                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
                 startActivity(intent);
             }
         });
---
+++ app/src/test/java/com/moringaschool/myrestraurants/MainActivityTest.java
@@ -0,0 +1,16 @@
+//package com.moringaschool.myrestraurants;
+//
+//import org.junit.Rule;
+//
+//import androidx.test.rule.ActivityTestRule;
+//import androidx.test.runner.AndroidJUnit4;
+//
+//import org.junit.Rule;
+//import org.junit.runner.RunWith;
+//
+//@RunWith(AndroidJUnit4.class)
+//public class RestaurantsActivityInstrumentationTest {
+//    @Rule
+//    public ActivityTestRule<RestaurantsActivity> activityTestRule =
+//            new ActivityTestRule<>(RestaurantsActivity.class);
+//}
